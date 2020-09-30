7a8783ded3045dbad703737909298f6f67cda491
Chien-Ping Hsieh <routerhsieh@gmail.com>
2020-09-29 20:51:04 -0700

    Change example programm to list all net namespace and dump interface list

--- example/main.go
+++ example/main.go
@@ -1,30 +1,113 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net"
+	"os"
+	"os/signal"
+	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/vishvananda/netns"
 )
 
-func main() {
+type NsMap map[int]int
+
+func getNsMap() (NsMap, error) {
+	procPaths, err := filepath.Glob("/proc/[0-9]*")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	m := NsMap{}
+	for _, procPath := range procPaths {
+		_, pidStr := filepath.Split(procPath)
+		identifierStr, err := os.Readlink(procPath + "/ns/net")
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		identifierStr = strings.Split(identifierStr, ":")[1]
+		identifierStr = identifierStr[1 : len(identifierStr)-1]
+
+		pid, err := strconv.Atoi(pidStr)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		identifier, err := strconv.Atoi(identifierStr)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		v, ok := m[identifier]
+		if !ok || pid < v {
+			m[identifier] = pid
+		}
+	}
+	return m, nil
+}
+
+func getInterfaces(ctx context.Context, handle netns.NsHandle, identifier int) {
 	// Lock the OS Thread so we don't accidentally switch namespaces
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
-	// Save the current network namespace
-	origns, _ := netns.Get()
-	defer origns.Close()
+	err := netns.Set(handle)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer handle.Close()
 
-	// Create a new network namespace
-	newns, _ := netns.New()
-	defer newns.Close()
+	sleepTime := time.Second * 3
+	timer := time.NewTimer(sleepTime)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-timer.C:
+			ifaces, err := net.Interfaces()
+			if err == nil {
+				log.Printf("Interfaces at identifier %d: %v\n\n", identifier, ifaces)
+			}
+			timer = time.NewTimer(sleepTime)
+		}
+	}
+}
 
-	// Do something with the network namespace
-	ifaces, _ := net.Interfaces()
-	fmt.Printf("Interfaces: %v\n", ifaces)
+func callAtExit(f func()) {
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		log.Println("Caught interrupt")
+		f()
+	}()
+}
 
-	// Switch back to the original namespace
-	netns.Set(origns)
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	callAtExit(cancel)
+	m, err := getNsMap()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Printf("%d namespaces...\n", len(m))
+	for netIdentifier, pid := range m {
+		handle, err := netns.GetFromPid(pid)
+		if err != nil {
+			log.Printf("Can't get namespace handle from %d:%d\n,", netIdentifier, pid)
+			continue
+		}
+		go getInterfaces(ctx, handle, netIdentifier)
+	}
+	<-ctx.Done()
+	time.Sleep(time.Millisecond * 100)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/vishvananda/netns
 
 go 1.12
 
-require golang.org/x/sys v0.0.0-20200217220822-9197077df867
+require (
+	github.com/go-delve/delve v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20200217220822-9197077df867
+)
